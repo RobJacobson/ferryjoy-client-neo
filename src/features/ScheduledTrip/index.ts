5e0a75e71aafc4673d113366e877e5c3401a4137
Rob Jacobson <rljacobson@outlook.com>
2026-02-03 15:58:50 -0800

    chore(scheduledTrips): remove ScheduledTrip components
    - Deleted ScheduledTripCard, ScheduledTripList, ScheduledTripTimeline, and their index file to streamline the scheduled trips feature.
    - This cleanup is part of a refactor to improve code maintainability and reduce complexity in the scheduled trips module.

--- src/features/ScheduledTrip/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Index file for ScheduledTrip feature.
- * Exports components for use in other parts of the application.
- */
-
-export * from "./ScheduledTripCard";
-export * from "./ScheduledTripList";
-export * from "./ScheduledTripTimeline";
